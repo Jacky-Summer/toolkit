387d68a1fc4d634702b883634b05fa3252aeb8ec
JackySummer <1106619871@qq.com>
2022-09-05 22:21:53 +0800

    chore: support use config package commintlintConfig

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,1 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['build', 'ci', 'chore', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'],
-    ],
-  },
-}
+module.exports = require('@summer-toolkit/config').commitlintConfig()
